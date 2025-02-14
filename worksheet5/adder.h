216e301399b6ef4a7f6d6ea1d21366a94bc96f7d
eeyzr1 <eeyzr1@nottingham.ac.uk>
2025-02-14 12:56:28 +0000

    WS5 ex2

---
+++ worksheet5/adder.h
@@ -0,0 +1,20 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** 
+ * @file adder.h
+ * @brief 这个文件包含 maths 库中所有导出的函数声明。
+ */
+
+/**
+ * @brief 加法函数
+ * 
+ * 这个函数用于计算两个整数的和。
+ * 
+ * @param a 第一个整数
+ * @param b 第二个整数
+ * @return 两数之和
+ */
+int add(int a, int b);
+
+#endif // MATHSLIB_ADDER_H
